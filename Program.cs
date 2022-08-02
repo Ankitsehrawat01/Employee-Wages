7627de06e77a2fec8eebcf895c12d3ab9788ce75
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-02 16:20:52 +0530

    switch case program

--- Program.cs
+++ Program.cs
@@ -1,27 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
-int isFullTime = 1;
-int isPartTime = 2;
-int wagePerHr = 20;
+const int isFullTime = 1;
+const int isPartTime = 2;
+const int wagePerHr = 20;
 int empHrs = 0;
 int empDailyWage = 0;
 Random random = new Random();
 int result = random.Next(0,3);
-if(result == isFullTime)
-    {
+switch (result)
+    { 
+    case isFullTime:
         Console.WriteLine("Employee is Full Time");
         empHrs = 8;
-        
-    }
-else if(result == isPartTime)
-    {
-    Console.WriteLine("Employee is Part time");
-    empHrs = 4; 
-}
-else
-    {
+        break;
+    case isPartTime:
+        Console.WriteLine("Employee is Part time");
+        empHrs = 4;
+        break;
+    default:
         Console.WriteLine("Employee is Absent");
         empHrs = 0;
+        break;
     }
 empDailyWage = empHrs * wagePerHr;
 Console.WriteLine("empDailyWage = " + empDailyWage);
