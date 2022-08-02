bbfcba3406aaed5ee30ca1b96c4f6e8862b4c275
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-02 14:29:28 +0530

    Employee wage Program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+int isPresent = 1;
+Random random = new Random();
+int result = random.Next(0,2);
+if(result==isPresent)
+    {
+        Console.WriteLine("Employee is Present");
+    }
+else
+    {
+        Console.WriteLine("Employee is Absent");
+    }
