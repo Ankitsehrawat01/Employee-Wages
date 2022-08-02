3ead601d2941d13758a81528cb99974d44978ef0
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-02 15:38:21 +0530

    UC3_Part_Time_Employee_and_Wage

--- Program.cs
+++ Program.cs
@@ -1,17 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
-int isPresent = 1;
+int isFullTime = 1;
+int isPartTime = 2;
 int wagePerHr = 20;
 int empHrs = 0;
 int empDailyWage = 0;
 Random random = new Random();
-int result = random.Next(0,2);
-if(result == isPresent)
+int result = random.Next(0,3);
+if(result == isFullTime)
     {
-        Console.WriteLine("Employee is Present");
+        Console.WriteLine("Employee is Full Time");
         empHrs = 8;
         
     }
+else if(result == isPartTime)
+    {
+    Console.WriteLine("Employee is Part time");
+    empHrs = 4; 
+}
 else
     {
         Console.WriteLine("Employee is Absent");
