4fd63b24724b6781dba843a68d81205f4925a846
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-16 18:27:50 +0530

    Wage Calculation for multiple companies

--- Employee.cs
+++ Employee.cs
@@ -10,44 +10,34 @@ namespace Employee_Wage
     {
         const int isFullTime = 1;
         const int isPartTime = 2;
-        const int wagePerHr = 20;
-        const int WorkingDays = 20;
-        const int maxHrsInMonth = 100;
-        public static int ComputeWage()
+        public static int ComputeWage(string company, int empRatePerHour, int WorkingDays, int maxHrsInMonth)
         {
             int empHrs = 0;
-            int empDailyWage = 0;
-            int totalEarnings = 0;
             int totalEmpHrs = 0;
             int totalWorkingDays = 0;
-            Random random = new Random();
-            int result = random.Next(0, 3);
             while (totalEmpHrs < maxHrsInMonth && totalWorkingDays <= WorkingDays)
             {
                 totalWorkingDays++;
+                Random random = new Random();
+                int result = random.Next(0, 3);
                 switch (result)
                 {
                     case isFullTime:
-                        Console.WriteLine("Employee is Full Time");
                         empHrs = 8;
                         break;
                     case isPartTime:
-                        Console.WriteLine("Employee is Part time");
                         empHrs = 4;
                         break;
                     default:
-                        Console.WriteLine("Employee is Absent");
                         empHrs = 0;
                         break;
                 }
                 totalEmpHrs += empHrs;
-                Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs = " + empHrs);
-                empDailyWage = empHrs * wagePerHr;
-                Console.WriteLine("empDailyWage = " + empDailyWage);
-                totalEarnings += empDailyWage;
+                Console.WriteLine("Days: " + totalWorkingDays + "Emp Hrs " + empHrs);
             }
-            Console.WriteLine("Total Earnings = " + totalEarnings);
-            return totalEarnings;
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+            return totalEmpWage;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using Employee_Wage;
 Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
 
-Employee.ComputeWage();
-
+Employee.ComputeWage("Infosys", 20, 2, 10);
+Employee.ComputeWage("TCS", 10, 5, 30);
